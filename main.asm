4a1a388e8eb635d06b783bfcc19d5d3c337fb4f3
Adam Kubiczek <adam@kubiczek.eu>
2023-09-11 22:40:30 +0200

    Refactor; shift text at VSYNC

--- main.asm
+++ main.asm
@@ -12,7 +12,7 @@ message:
         .charmap $41 + i, $41 + i - $40
     .endrepeat
 
-    .asciiz "                                           1234567890+ABC STRING PLUS! THIS WHAT I MEAN TO BE, GREAT, GREAT IDEA :)                                                                                "
+    .asciiz "X   X                                      THIS IS NOT COMMODORE 64, THIS IS COMMANDER X16. MODERN, YET 8-BIT COMPUTER!                                                                            "
 
 default_irq_handler: .addr $0000
 
@@ -21,33 +21,101 @@ ADDRx_M = $9f21
 ADDRx_H = $9f22
 VDATA_0 = $9f23
 
-;     ldy #$00
-; @outer_loop:
-;     lda #$00
-; @loop:
-;     inc
-;     cmp #$ff
-;     bne @loop
-;     iny
-;     cpy #$0a
-;     bne @outer_loop
-
 .segment "CODE"
 
     jsr reset_text_pointer
+    jsr copy_text_to_screen
 
     stz h_scroll
     stz should_shift
 
     jsr set_custom_irq_handler
-    jsr copy_text_to_screen
     rts ; exit to basic
+
+; ----------------------------------------------- ;
+set_custom_irq_handler:
+    ; set my handler
+    sei
+    lda #<custom_irq_handler
+    sta $0314
+    lda #>custom_irq_handler
+    sta $0315
+
+    ; enable LINE and VSYNC interrupt
+    lda #%00000011 ; set LINE and VSYNC bits in IEN register
+    sta $9F26    
+
+    ; set interrupt to line $1D6
+    lda #$D6
+    sta $9F28 ;IRQLINE_L (Write only)
+    lda $9F26 
+    ora #%10000000  
+    sta $9F26 ;IRQLINE_H (bit 8)
+
+    cli
+    rts
+
+; ----------------------------------------------- ;
+custom_irq_handler:
+    lda $9F27
+    and #%00000001 ; sets zero-flag when bit 0 at $9F27 is cleared (not VSYNC interrupt)
+    bne vsync_interrupt
+
+    jsr scroll_text
+
+    lda #%00000010 ; clear LINE interrupt status
+    sta $9F27
+
+    ply
+    plx
+    pla
+    rti
+
+; ----------------------------------------------- ;
+vsync_interrupt:
+    stz $9F37 ; reset H-SCROLL
+
+    lda should_shift
+    cmp #$ff
+    bne @skip_shift_chars
+    jsr shift_characters
+
+@skip_shift_chars:
+
+    lda #%00000001 ; clear VSYNC interrupt status
+    sta $9F27
+
+    ply
+    plx
+    pla
+    rti 
+
+scroll_text:
+    lda h_scroll
+    cmp #$08
+    bne @skip
+    lda #$ff
+    sta should_shift
+    stz h_scroll
+    rts
+@skip:
+    inc
+    sta h_scroll
+    sta $9F37 ;scroll screen 
+    rts
+
+; shifts characters by one to the left, and resets h_scroll
+shift_characters:
+    stz should_shift
+    jsr increase_text_pointer
+    jsr copy_text_to_screen
+    rts
 
 increase_text_pointer:
     lda message_pointer
     inc
     sta message_pointer
-    bne @skip
+    bne @skip ; goto @skip if message_pointer has not changed to $00
     lda message_pointer+1
     inc
     sta message_pointer+1
@@ -84,116 +152,3 @@ reset_text_pointer:
     lda #>message
     sta message_pointer+1
     rts
-
-scroll_text:
-    lda h_scroll
-    cmp #$08
-    bne @skip
-    lda #$ff
-    sta should_shift
-    rts
-@skip:
-    inc
-    sta h_scroll
-    sta $9F37
-    rts
-
-shift_characters:
-    stz should_shift
-    stz h_scroll
-    jsr increase_text_pointer
-    jsr copy_text_to_screen
-    rts
-
-set_custom_irq_handler:
-    ; preserve_default_irq
-    lda $0314
-    sta default_irq_handler
-    lda $0315
-    sta default_irq_handler+1
-
-    ; set my handler
-    sei
-    lda #<custom_irq_handler
-    sta $0314
-    lda #>custom_irq_handler
-    sta $0315
-
-    ; set_interrupt_to_line_xxx:
-    lda #$D6
-    sta $9F28
-    lda $9F26
-    ora #%10000000
-    sta $9F26
-
-    ; enable_line_interrupt:
-    lda $9F26
-    ora #$02
-    sta $9F26
-
-    cli
-    rts
-
-custom_irq_handler:
-    lda $9F27
-    and #$02
-    beq vsync_interrupt
-
-    ; Whatever code you need to do
-    ; on the line interrupt...
-
-    lda int_line
-    cmp #$ff
-    beq odd_interrupt
-    jsr scroll_text
-    lda #$ff
-    sta int_line
-    ; set_interrupt_to_line_yyy:
-    sei
-    lda #$10
-    sta $9F28
-    lda $9F26
-    ora #%10000000
-    sta $9F26
-    cli
-    jmp skip12
-odd_interrupt:
-    stz int_line
-    ; set_interrupt_to_line_xxx:
-    lda #$D6
-    sta $9F28
-    lda $9F26
-    ora #%10000000
-    sta $9F26    
-skip12:
-    lda #$02
-    sta $9F27
-    ply
-    plx
-    pla
-    rti
-
-vsync_interrupt:
-    lda $9F27
-    and #$01
-    beq irq_done
-
-    ; Whatever code your program
-    ; wanted to execute...        
-    ; stz $9F37
-
-    lda should_shift
-    cmp #$ff
-    bne @skip
-    jsr shift_characters
-    
-
-@skip:
-    lda #$01
-    sta $9F27
-irq_done:   
-    ply
-    plx
-    pla
-    rti 
-    jmp (default_irq_handler)
